ec97f6608263808ea97e524c62f5b0f3a855db3c
radwa-gamal30 <radwagamal482@gmail.com>
2024-09-07 02:44:04 +0300

    setting files of app component.ts routes sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav'
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    CommonModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    SidebarComponent,
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { AddGroupComponent } from './components/add-group/add-group.component';
+import { AddHolidayComponent } from './components/add-holiday/add-holiday.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'addgroup',component: AddGroupComponent, title:'Add Group'},
+    {path: 'addholiday',component: AddHolidayComponent, title:'Add Holiday'},
+    // {path:''}
+];
