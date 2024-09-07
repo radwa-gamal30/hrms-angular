1dd4b96b7f3cbb357e6e739368c2f5fc99d255c9
hazemelwaleed45 <hazemelwaleed45@gmail.com>
2024-09-07 15:16:51 +0300

    General Setting Page design done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
+import { GeneralSettingsComponent } from './components/general-settings/general-settings.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet , GeneralSettingsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
