9da072abefec7707211aec84c440003d91252b30
Moshaheen5 <muhammadashrf258@gmail.com>
2024-09-08 17:54:07 +0300

    not finished yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { GeneralSettingsComponent } from './components/general-settings/general-settings.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddHolidayComponent } from './components/add-holiday/add-holiday.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , GeneralSettingsComponent],
+  imports: [RouterOutlet , GeneralSettingsComponent,AddHolidayComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
