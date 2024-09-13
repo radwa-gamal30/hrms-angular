91227b4610860b1fb53e79f6d2b337c79a97a6d9
radwa-gamal30 <radwagamal482@gmail.com>
2024-09-13 22:51:26 +0300

    change inlayout

--- src/app/components/add-user1/add-user1.component.ts
+++ src/app/components/add-user1/add-user1.component.ts
@@ -1,9 +1,19 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
+import { MatFormField } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-add-user1',
   standalone: true,
-  imports: [],
+  imports: [MatFormField,
+    FormsModule,
+    MatInputModule,
+    MatSelectModule,
+    NgStyle
+  ],
   templateUrl: './add-user1.component.html',
   styleUrl: './add-user1.component.css'
 })
