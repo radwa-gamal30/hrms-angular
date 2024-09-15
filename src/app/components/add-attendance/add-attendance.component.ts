e793134750173cb0b979a9ddf32f2e2c9e63ba13
radwa-gamal30 <radwagamal482@gmail.com>
2024-09-15 19:44:36 +0300

    add attendnace full

--- src/app/components/add-attendance/add-attendance.component.ts
+++ src/app/components/add-attendance/add-attendance.component.ts
@@ -1,19 +1,82 @@
+import { AttendanceService } from './../../Services/attendnace/attendance.service';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { provideHttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { LoaderComponent } from "../loader/loader.component";
 
 @Component({
   selector: 'app-add-attendance',
   standalone: true,
-  imports: [RouterModule,CommonModule,FormsModule],
   templateUrl: './add-attendance.component.html',
-  styleUrl: './add-attendance.component.css'
+  imports: [RouterModule, CommonModule, FormsModule, LoaderComponent],
+  styleUrl: './add-attendance.component.css',
 })
 export class AddAttendanceComponent {
-departureTime!: any;
-attedanceTime!: any;
-department_name!: any;
-employee_name!: any;
+  employees:any[]=[];
+  selectedEmployeeId: string = '';  
+  attedanceTime: string = '';  
+  departureTime: string = ''; 
+  attendanceDate: Date | undefined;  
+  errors: any = {};   
+  isLoading:boolean=false;
+  loadingTitle:string='Loading';
 
+
+  constructor(private attendanceService: AttendanceService) {}
+
+ngOnInit(){
+  this.loadEmployees();
+}
+loadEmployees(){
+  this.attendanceService.getEmployees()
+    .subscribe({
+      next: (em:any) => {
+        this.employees =em;
+        this.isLoading=false;
+       },
+      error: (err:any) => {
+        this.errors=err.error.errors ;
+        this.isLoading=false;
+        console.log(err.error.errors,'errors');}
+        
+    });
+  
+}
+
+
+submitAttendance() {
+  this.isLoading=true;
+  this.loadingTitle='saving';
+  var inputs = {
+    employee_id: this.selectedEmployeeId,
+    check_in: this.attedanceTime,
+    check_out: this.departureTime,
+    date: this.attendanceDate,
+  };
+
+  this.attendanceService.submitAttendance(inputs).subscribe({
+    next: (res: any) => {
+      console.log(res, 'response');
+      
+      alert(res.message);
+      this.selectedEmployeeId='';
+      this.attedanceTime='';
+      this.departureTime='';
+      this.attendanceDate=new Date();
+      this.isLoading=false;
+
+
+    },
+    error: (er: any) => {
+      
+      this.errors = er.error.errors; 
+      this.isLoading=false;
+      console.log(er.error.errors, 'errors');
+    }
+  });
+}
+ 
+  
 }
